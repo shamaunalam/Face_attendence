dae8fdf8e56bc21237ecc9cf4b2b17ced0f9b52c
shamaunalam <shamaunalam@gmail.com>
2020-11-26 16:33:04 +0530

    fourth commit

--- GUI.py
+++ GUI.py
@@ -8,7 +8,6 @@ Created on Thu Nov 26 16:12:49 2020
 from PyQt5 import  QtWidgets,uic
 
 
-
 def gotostudentlogin():
     homePage.close()
     studentLoginPage.show()
@@ -24,22 +23,35 @@ def backfromfaclogin():
 def backfromstudentlogin():
     studentLoginPage.close()
     homePage.show()
-    
+
+def gotofacultydashboard():
+    facultyLoginPage.close()
+    facultyDashboard.show()    
+
+def backtofacultylogin():
+    facultyDashboard.close()
+    facultyLoginPage.show()
+
 
 app = QtWidgets.QApplication([])
 
 homePage = uic.loadUi('Uifiles/FrontPage.ui')
 studentLoginPage = uic.loadUi('Uifiles/student_login.ui')
 facultyLoginPage = uic.loadUi("Uifiles/faculty_login.ui")
+facultyDashboard = uic.loadUi("Uifiles/faculty_dashboard.ui")
+
 
 homePage.show()
 homePage.studentLoginBut.clicked.connect(gotostudentlogin)
 homePage.facultyLoginBut.clicked.connect(gotofacultylogin)
 
-
 facultyLoginPage.backBut.clicked.connect(backfromfaclogin)
+facultyLoginPage.loginBut.clicked.connect(gotofacultydashboard)
 
 
 studentLoginPage.backBut.clicked.connect(backfromstudentlogin)
 
+
+
+facultyDashboard.backBut.clicked.connect(backtofacultylogin)
 app.exec()
